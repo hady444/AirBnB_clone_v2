858892a11e389bfcfec59cc529970d62b66475f2
hady444 <hady444@github.com>
2024-03-15 23:57:08 +0200

    task 0

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -15,32 +15,33 @@ Holeborton School
 </body>
 </html>
 " > /data/web_static/releases/test/index.html
-ln -sf /data/web_static/releases/test/ /data/web_static/current
+ln -sf /data/web_static/releases/test /data/web_static/current
 
 sudo chown -R ubuntu /data
 sudo chgrp -R ubuntu /data
 
 echo "
 server {
-	listen 80 default_server;
-	listen [::]:80 default_server;
-	add_header X-Served-By $HOSTNAME;
-	root /var/www/html;
-	index index.html index.htm;
+        listen 80 default_server;
+        listen [::]:80 default_server;
+        add_header X-Served-By \$HOSTNAME;
+        root /var/www/html;
+        index index.html index.htm;
 
-	location /hbnb_static {
-        	alias /data/web_static/current;
-        	index index.html index.htm;
-	}
+        location /hbnb_static {
+                alias /data/web_static/current;
+                index index.html index.htm;
+        }
 
-	location /redirect_me {
-	retrun 301 https://www.youtube.com/@ALXAfrica;
-	}
-	error_page 404 /error_404.html;
-	location /error_404 {
-	  root /var/www/html;
-	  internal;
-	}
+        location /redirect_me {
+            return 301 https://www.youtube.com/@ALXAfrica;
+        }
+
+        error_page 404 /error_404.html;
+        location = /error_404.html {
+          root /var/www/html;
+          internal;
+        }
 }
 " > /etc/nginx/sites-available/default
 sudo systemctl restart nginx
