d626c8eaa01229ff8c68e525efbbeeb22b153433
hady444 <hady444@github.com>
2024-03-15 23:42:05 +0200

    task 0

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -22,20 +22,40 @@ sudo chown -R ubuntu /data
 sudo chgrp -R ubuntu /data
 
 echo "
-server {
-	listen 80 default_server;
-	listen [::]:80 default_server;
-	add_header X-Served-By $HOSTNAME;
-	root /var/www/html;
-	index index.html index.htm index.nginx-debian.html;
+listen 80 default_server;
+    listen [::]:80 default_server;
+    add_header X-Served-By $HOSTNAME;
+    root   /var/www/html;
+    index  index.html index.htm;
 
-	location /hbnb_static {
-        	alias /data/web_static/current;
-        	index index.html index.htm;
-	}
-	rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;
-        error_page 404 /error_404.html;
-}
+    location /hbnb_static {
+        alias /data/web_static/current;
+        index index.html index.htm;
+    }
+
+    location /redirect_me {
+        return 301 http://cuberule.com/;
+    }
+
+    error_page 404 /404.html;
+    location /404 {
+      root /var/www/html;
+      internal;
+    }
 " > /etc/nginx/sites-available/default
+#server {
+	#listen 80 default_server;
+	#listen [::]:80 default_server;
+	#add_header X-Served-By $HOSTNAME;
+	#root /var/www/html;
+	#index index.html index.htm index.nginx-debian.html;
+
+	#location /hbnb_static {
+        #	alias /data/web_static/current;
+        #	index index.html index.htm;
+	#}
+	#rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;
+        #error_page 404 /error_404.html;
+#}
 
 sudo systemctl restart nginx
