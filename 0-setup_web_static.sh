7d72c38c7181bfc288a2f7b1b898cb92d87bee99
hady444 <hady444@github.com>
2024-03-15 23:30:52 +0200

    task 0

---
+++ 0-setup_web_static.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env bash
+#Bash script that sets up your web servers for the deployment of web_static
+
+sudo apt-get update
+sudo apt-get -y install nginx
+mkdir -p /data/web_static/releases/test
+mkdir -p /data/web_static/shared
+cd releases && mkdir test && cd test
+echo '
+<!DOCTYPE HTML>
+<head>
+</head>
+<body>
+Holeborton School
+</body>
+</html>
+' > /data/web_static/releases/test/index.html
+link_path="/data/web_static/current"
+target_path="/data/web_static/releases/test/"
+
+ln -sf "$target_path" "$link_path"
+sudo chown -R ubuntu /data
+sudo chgrp -R ubuntu /data
+
+echo '
+server {
+	listen 80 default_server;
+	listen [::] 80 default_server;
+	add_header X-Served-By $HOSTNAME;
+	root /var/www/html;
+	location /hbnb_static {
+        	alias /data/web_static/current;
+        	index index.html index.htm;
+	}
+	rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;
+        error_page 404 /error_404.html;
+}
+' > /etc/nginx/sites-available/default
+
+service nginx restart
---
+++ fabfile.py
@@ -0,0 +1,3 @@
+#!/usr/bin/python3
+def hello(who="world"):
+    print("Hello {who}".format(who=who))
