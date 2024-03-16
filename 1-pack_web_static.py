134040d97d0aecb70be92364d643064ae6dfd64f
hady444 <hady444@github.com>
2024-03-16 17:58:04 +0200

    task 1

---
+++ 1-pack_web_static.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+""" Fabric script that generates a .tgz archive
+    from the contents of the web_static
+"""
+
+from Fabric import Connection
+import os.path
+from datetime import datetime
+def do_pack():
+    date = datetime.utcnow()
+    file_name = "versions/web_static_{}{}{}{}{}{}.tgz".format(
+            date.year, date.month, date.day, date.hour, date.minute,
+            date.second)
+    if (os.path..isdir('versions') == False):
+        if (local('mkdir -p versions').failed == True):
+            return None
+    if local('tar -cvzf {} web_static'.format(file_name)).failed == True:
+        return None
+    return file_name
