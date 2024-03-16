d43564faf40dab14578315b7600d3952bd88bfca
hady444 <hady444@github.com>
2024-03-16 18:00:00 +0200

    task 1

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -3,17 +3,20 @@
     from the contents of the web_static
 """
 
+
 from Fabric import Connection
 import os.path
 from datetime import datetime
+
+
 def do_pack():
     date = datetime.utcnow()
     file_name = "versions/web_static_{}{}{}{}{}{}.tgz".format(
             date.year, date.month, date.day, date.hour, date.minute,
             date.second)
-    if (os.path..isdir('versions') == False):
-        if (local('mkdir -p versions').failed == True):
+    if (os.path.isdir('versions') is False):
+        if (local('mkdir -p versions').failed is True):
             return None
-    if local('tar -cvzf {} web_static'.format(file_name)).failed == True:
+    if local('tar -cvzf {} web_static'.format(file_name)).failed is True:
         return None
     return file_name
