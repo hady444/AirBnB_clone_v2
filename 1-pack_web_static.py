2a68a9a796d3f97b96a9d1f84449c9d35b45371c
hady444 <hady444@github.com>
2024-03-16 18:04:43 +0200

    task 1

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -12,12 +12,12 @@ from datetime import datetime
 def do_pack():
     """ function to archive web static file """
     date = datetime.utcnow()
-    file_name = "versions/web_static_{}{:02d}{:02d}{:02d}{:02d}{:02d}.tgz".format(
+    f_name = "versions/web_static_{}{:02d}{:02d}{:02d}{:02d}{:02d}.tgz".format(
             date.year, date.month, date.day, date.hour, date.minute,
             date.second)
     if (os.path.isdir('versions') is False):
         if (local('mkdir -p versions').failed is True):
             return None
-    if local('tar -cvzf {} web_static'.format(file_name)).failed is True:
+    if local('tar -cvzf {} web_static'.format(f_name)).failed is True:
         return None
-    return file_name
+    return f_name
