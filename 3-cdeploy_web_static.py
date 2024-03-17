85b18c156ba3beb14b49a98faf28fe6e1b4a91d4
hady444 <hady444@github.com>
2024-03-17 02:58:28 +0200

    task 3

--- 3-cdeploy_web_static.py
+++ 3-cdeploy_web_static.py
@@ -2,13 +2,15 @@
 # Full deployment
 
 
+from os.path import exists
 do_pack = __import__('1-pack_web_static').do_pack
 do_deploy = __import__('2-do_deploy_web_static').do_deploy
 
 
 def deploy():
     """ Full deployment """
-    archive_file = do_pack()
-    if archive_file is None:
+    new_archive_path = do_pack()
+    if exists(new_archive_path) is False:
         return False
-    return do_deploy(archive_file)
+    result = do_deploy(new_archive_path)
+    return result
