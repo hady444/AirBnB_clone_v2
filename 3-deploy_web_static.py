4cc0db5864f1a9fecf56e14da960cffa1752d515
hady444 <hady444@github.com>
2024-03-17 03:05:26 +0200

    task 3

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -1,14 +1,55 @@
 #!/usr/bin/python3
-# Full deployment
+""" Fabric script that creates and distributes an archive to
+web servers, using the function deploy """
 
 
+from fabric.api import *
+from datetime import datetime
 from os.path import exists
-do_pack = __import__('1-pack_web_static').do_pack
-do_deploy = __import__('2-do_deploy_web_static').do_deploy
+
+
+env.hosts = ['35.237.166.125', '54.167.61.201']
+
+
+def do_pack():
+    """.tgz archive from the contents of the web_static folder
+    """
+    local("sudo mkdir -p versions")
+    date = datetime.now().strftime("%Y%m%d%H%M%S")
+    filename = "versions/web_static_{}.tgz".format(date)
+    result = local("sudo tar -cvzf {} web_static".format(filename))
+    if result.succeeded:
+        return filename
+    else:
+        return None
+
+
+def do_deploy(archive_path):
+    """ distributes an archive to my web servers
+    """
+    if exists(archive_path) is False:
+        return False
+    filename = archive_path.split('/')[-1]
+    no_tgz = '/data/web_static/releases/' + "{}".format(filename.split('.')[0])
+    tmp = "/tmp/" + filename
+
+    try:
+        put(archive_path, "/tmp/")
+        run("mkdir -p {}/".format(no_tgz))
+        run("tar -xzf {} -C {}/".format(tmp, no_tgz))
+        run("rm {}".format(tmp))
+        run("mv {}/web_static/* {}/".format(no_tgz, no_tgz))
+        run("rm -rf {}/web_static".format(no_tgz))
+        run("rm -rf /data/web_static/current")
+        run("ln -s {}/ /data/web_static/current".format(no_tgz))
+        return True
+    except e:
+        return False
 
 
 def deploy():
-    """ Full deployment """
+    """ creates and distributes an archive to web servers
+    """
     new_archive_path = do_pack()
     if exists(new_archive_path) is False:
         return False
